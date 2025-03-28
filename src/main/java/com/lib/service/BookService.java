51a95908ce13b4b0111ba6d9f26debc6d0ea5ebf
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-03-28 15:39:58 +0530

    Update BookService.java

--- src/main/java/com/lib/service/BookService.java
+++ src/main/java/com/lib/service/BookService.java
@@ -6,7 +6,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import java.util.stream.Collectors;
 import com.lib.model.Book;
 import com.lib.model.BookTransaction;
 import com.lib.model.User;
