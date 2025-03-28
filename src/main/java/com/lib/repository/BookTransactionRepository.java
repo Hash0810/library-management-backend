51a4f258b7514300a78e2715135628aba5d240dd
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-03-28 15:31:49 +0530

    Update BookTransactionRepository.java

--- src/main/java/com/lib/repository/BookTransactionRepository.java
+++ src/main/java/com/lib/repository/BookTransactionRepository.java
@@ -2,12 +2,13 @@ package com.lib.repository;
 
 import java.util.List;
 import java.util.Optional;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lib.model.BookTransaction;
 
 public interface BookTransactionRepository extends JpaRepository<BookTransaction, Integer> {
-	List<BookTransaction> findByUser_Username(String username);
+	Page<BookTransaction> findByUser_Username(String username, Pageable pageable);
 	Optional<BookTransaction> findByBook_IdAndUser_UsernameAndReturnDateIsNull(Integer bookId, String username);
 }
