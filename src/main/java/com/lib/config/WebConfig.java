ec33bfb07327e32158cd100d915d50d22810baf4
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-03-28 15:49:06 +0530

    Update WebConfig.java

--- src/main/java/com/lib/config/WebConfig.java
+++ src/main/java/com/lib/config/WebConfig.java
@@ -2,8 +2,11 @@ package com.lib.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-// In your WebConfig.java
+import org.springframework.data.web.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.PageSerializationMode;
+
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
