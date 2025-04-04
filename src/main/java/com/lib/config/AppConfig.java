8f85dd09e1f52cf73f80c9efba5d8caf50060377
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-04 18:29:24 +0530

    Update AppConfig.java

--- src/main/java/com/lib/config/AppConfig.java
+++ src/main/java/com/lib/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.lib.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Primary;
+
 
 @Configuration
 public class AppConfig {
