b1f7b80f0d8f49622357169aefda9bf31026668a
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-04 17:51:48 +0530

    Update Librarian.java

--- src/main/java/com/lib/model/Librarian.java
+++ src/main/java/com/lib/model/Librarian.java
@@ -3,19 +3,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.DiscriminatorValue;
 
 @Entity
+@DiscriminatorValue("LIBRARIAN")
 public class Librarian extends User {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
-
-	@Override
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
+	
 }
