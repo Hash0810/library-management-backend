8cf0fa77d471d3451edec24fc6416b2f149259e0
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-05 14:32:05 +0530

    Update WebConfig.java

--- src/main/java/com/lib/config/WebConfig.java
+++ src/main/java/com/lib/config/WebConfig.java
@@ -1,4 +1,5 @@
 package com.lib.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,9 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Adjust the path as needed
-                .allowedOrigins("http://localhost:3000") // Your frontend URL
+        registry.addMapping("/**")
+                .allowedOrigins("https://library-management-system-fe-phi.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true); // Optional: Only needed if you're using cookies
     }
 }
