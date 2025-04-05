0330298fd8f5dab74c917f12010e79c80eb0f90d
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-05 10:31:29 +0530

    Update UserController.java

--- src/main/java/com/lib/controller/UserController.java
+++ src/main/java/com/lib/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +27,6 @@ import com.lib.model.User;
 import com.lib.service.BookService;
 import com.lib.service.FineService;
 import com.lib.service.UserService;
-import Page;
 import jakarta.validation.Valid;
 
 @RestController
