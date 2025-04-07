b599151211d963ab5827937ee8735a406113b154
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-07 12:12:42 +0530

    Update UserRepository.java

--- src/main/java/com/lib/repository/UserRepository.java
+++ src/main/java/com/lib/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.lib.model.Role;
 import com.lib.model.User;
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByUsername(String username);
     User findByUsername(String username);
     User findByEmail(String email);
