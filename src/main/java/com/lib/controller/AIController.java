be5c6369a605351e39521287ef93ba9d784c94ff
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-08 12:58:25 +0530

    Update AIController.java

--- src/main/java/com/lib/controller/AIController.java
+++ src/main/java/com/lib/controller/AIController.java
@@ -3,14 +3,13 @@ package com.lib.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.lib.service.AIService;
 import com.lib.service.SpringOpenService;
 @RestController
 @RequestMapping("/api/ai")
 public class AIController {
 
     
-    private AIService aiService;
+    
 
     
     private final SpringOpenService springOpenService;
