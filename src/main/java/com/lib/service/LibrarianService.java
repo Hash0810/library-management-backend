00807ba7207bfb378ed0fc0071bd881e0b3a851c
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-09 10:48:56 +0530

    Create LibrarianService.java

---
+++ src/main/java/com/lib/service/LibrarianService.java
@@ -0,0 +1,47 @@
+package com.lib.service;
+import com.lib.repository.BookTransactionRepository;
+import com.lib.repository.UserRepository;
+import com.lib.service.ReceiptService;
+import com.lib.service.EmailService;
+import com.lib.model.BookTransaction;
+import com.lib.model.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.zxing.WriterException;
+import com.itextpdf.text.DocumentException;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class LibrarianService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BookTransactionRepository bookTransactionRepository;
+
+    @Autowired
+    private ReceiptService receiptService;
+
+    @Autowired
+    private EmailService emailService;
+
+    public String sendReceiptToUser(String username) throws IOException, DocumentException, WriterException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        List<BookTransaction> transactions = bookTransactionRepository.findByUser_Username(username);
+        if (transactions.isEmpty()) {
+            throw new RuntimeException("No transactions found for user");
+        }
+
+        byte[] pdf = receiptService.generateReceiptPdf(transactions);
+        emailService.sendReceiptWithPDF(user.getEmail(), pdf, "Book_Receipt.pdf");
+
+        return "Receipt sent to email successfully.";
+    }
+}
