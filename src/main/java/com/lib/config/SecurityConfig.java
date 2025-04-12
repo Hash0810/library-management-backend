6e7d20790d1da03dc92f0ff983fc703a14f7368c
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-12 15:08:10 +0530

    Update SecurityConfig.java

--- src/main/java/com/lib/config/SecurityConfig.java
+++ src/main/java/com/lib/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
