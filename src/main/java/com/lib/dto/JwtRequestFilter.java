269930bfc133089e29dacdc0d5d7d05b50f363c4
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-13 10:17:20 +0530

    Update JwtRequestFilter.java

--- src/main/java/com/lib/dto/JwtRequestFilter.java
+++ src/main/java/com/lib/dto/JwtRequestFilter.java
@@ -5,13 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationFilter;
 import org.springframework.stereotype.Component;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import com.lib.util.JwtUtil;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
