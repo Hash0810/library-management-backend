495f715060b622c7a3a406972135c6adf894933a
Vunnam Hashwanth <71806128+Hash0810@users.noreply.github.com>
2025-04-26 10:25:36 +0530

    Update LibraryManagementRun.java

--- src/main/java/com/lib/LibraryManagementRun.java
+++ src/main/java/com/lib/LibraryManagementRun.java
@@ -3,8 +3,10 @@ package com.lib;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.sprongframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EntityScan(basePackages = {"com.lib.*"})
 public class LibraryManagementRun {
 
